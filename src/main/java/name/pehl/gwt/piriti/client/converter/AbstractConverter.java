bec15ae2a760fd435ece8cb10c6e8f89d00c95b1
Harald Pehl <harald.pehl@gmail.com>
2010-01-08 13:56:18 +0000

    Test commit

--- src/main/java/name/pehl/gwt/piriti/client/converter/AbstractConverter.java
+++ src/main/java/name/pehl/gwt/piriti/client/converter/AbstractConverter.java
@@ -3,8 +3,8 @@ package name.pehl.gwt.piriti.client.converter;
 /**
  * Base class for all converters with some common functionality.
  * 
- * @author $Author:$
- * @version $Revision$
+ * @author $LastChangedBy:$
+ * @version $LastChangedRevision$
  */
 public abstract class AbstractConverter<T> implements Converter<T>
 {
