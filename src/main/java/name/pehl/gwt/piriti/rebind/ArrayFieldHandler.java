52dbde9c72bf72c4280d9ae9ac230f8149f431a5
Harald Pehl <harald.pehl@gmail.com>
2010-01-11 16:34:15 +0000

    Started implementation

--- src/main/java/name/pehl/gwt/piriti/rebind/ArrayFieldHandler.java
+++ src/main/java/name/pehl/gwt/piriti/rebind/ArrayFieldHandler.java
@@ -1,18 +1,22 @@
 package name.pehl.gwt.piriti.rebind;
 
+import com.google.gwt.core.ext.typeinfo.JArrayType;
+import com.google.gwt.core.ext.typeinfo.JType;
+
 /**
  * {@link FieldHandler} for arrays. TODO Implement me!
  * 
- * @author $LastChangedBy:$
- * @version $LastChangedRevision:$
+ * @author $LastChangedBy$
+ * @version $LastChangedRevision$
  */
 public class ArrayFieldHandler extends AbstractFieldHandler
 {
     @Override
     public void write(IndentedWriter writer, FieldContext fieldContext)
     {
-        // assert fieldContext.isArray();
-        // JArrayType arrayType = fieldContext.getArrayType();
+        assert fieldContext.isArray();
+        JArrayType arrayType = fieldContext.getArrayType();
+        JType componentType = arrayType.getComponentType();
 
         writeComment(writer, fieldContext);
         writer.write("// Not yet implemented!");
