147bf0e033fdb18fc43cf7016cc7741e4ae6ecaf
Harald Pehl <harald.pehl@gmail.com>
2010-03-19 10:58:09 +0000

    First basic implementation (not tested)

--- gxt/src/main/java/name/pehl/piriti/gxt/rebind/json/JsonModelReaderCreator.java
+++ gxt/src/main/java/name/pehl/piriti/gxt/rebind/json/JsonModelReaderCreator.java
@@ -1,16 +1,17 @@
 package name.pehl.piriti.gxt.rebind.json;
 
-import name.pehl.piriti.client.json.JsonField;
-import name.pehl.piriti.rebind.AbstractReaderCreator;
+import name.pehl.piriti.gxt.client.json.JsonField;
+import name.pehl.piriti.gxt.client.json.JsonModel;
 import name.pehl.piriti.rebind.FieldContext;
+import name.pehl.piriti.rebind.FieldHandlerRegistry;
 import name.pehl.piriti.rebind.IndentedWriter;
 import name.pehl.piriti.rebind.fieldhandler.FieldHandler;
+import name.pehl.piriti.rebind.json.JsonReaderCreator;
 
 import com.google.gwt.core.ext.GeneratorContext;
 import com.google.gwt.core.ext.TreeLogger;
 import com.google.gwt.core.ext.UnableToCompleteException;
 import com.google.gwt.core.ext.typeinfo.JClassType;
-import com.google.gwt.core.ext.typeinfo.JField;
 
 /**
  * Class which generates the code necessary to map the annotated fields.
@@ -18,13 +19,19 @@ import com.google.gwt.core.ext.typeinfo.JField;
  * @author $LastChangedBy: harald.pehl $
  * @version $LastChangedRevision: 137 $
  */
-public class JsonModelReaderCreator extends AbstractReaderCreator
+public class JsonModelReaderCreator extends JsonReaderCreator
 {
     public JsonModelReaderCreator(GeneratorContext context, JClassType interfaceType, String implName,
             String readerClassname, TreeLogger logger) throws UnableToCompleteException
     {
         super(context, interfaceType, implName, readerClassname, logger);
-        this.handlerRegistry = new JsonModelFieldHandlerRegistry();
+    }
+
+
+    @Override
+    protected FieldHandlerRegistry setupFieldHandlerRegistry()
+    {
+        return new JsonModelFieldHandlerRegistry();
     }
 
 
@@ -32,204 +39,27 @@ public class JsonModelReaderCreator extends AbstractReaderCreator
     protected void createImports(IndentedWriter writer) throws UnableToCompleteException
     {
         super.createImports(writer);
-        writer.write("import com.google.gwt.json.client.*;");
-        writer.write("import name.pehl.piriti.client.json.*;");
+        writer.write("com.extjs.gxt.ui.client.data.*;");
     }
 
 
     @Override
-    protected void createMemberVariables(IndentedWriter writer) throws UnableToCompleteException
+    protected void processMappings(IndentedWriter writer, String jsonVariable) throws UnableToCompleteException
     {
-        super.createMemberVariables(writer);
-        writer.write("private JsonRegistry jsonRegistry;");
-    }
-
-
-    @Override
-    protected void createConstructorBody(IndentedWriter writer)
-    {
-        super.createConstructorBody(writer);
-        writer.write("this.jsonRegistry = JsonGinjector.INJECTOR.getJsonRegistry();");
-        writer.write("this.jsonRegistry.register(%s.class, this);", modelType.getQualifiedSourceName());
-    }
-
-
-    @Override
-    protected void createMethods(IndentedWriter writer) throws UnableToCompleteException
-    {
-        readFromString(writer);
-        writer.newline();
-
-        readFromJsonObject(writer);
-        writer.newline();
-
-        readListFromString(writer);
-        writer.newline();
-
-        readListFromJsonArray(writer);
-        writer.newline();
-
-        internalRead(writer);
-        writer.newline();
-    }
-
-
-    private void readFromString(IndentedWriter writer) throws UnableToCompleteException
-    {
-        writer.write("public %s read(String jsonString) {", modelType.getParameterizedQualifiedSourceName());
-        writer.indent();
-        writer.write("%s model = null;", modelType.getParameterizedQualifiedSourceName());
-        writer.write("if (jsonString != null && jsonString.trim().length() != 0) {");
-        writer.indent();
-        writer.write("JSONValue jsonValue = JSONParser.parse(jsonString);");
-        writer.write("if (jsonValue != null) {");
-        writer.indent();
-        writer.write("JSONObject jsonObject = jsonValue.isObject();");
-        writer.write("if (jsonObject != null) {");
-        writer.indent();
-        writer.write("model = internalRead(jsonObject);");
-        writer.outdent();
-        writer.write("}");
-        writer.outdent();
-        writer.write("}");
-        writer.outdent();
-        writer.write("}");
-        writer.write("return model;");
-        writer.outdent();
-        writer.write("}");
-    }
-
-
-    private void readFromJsonObject(IndentedWriter writer) throws UnableToCompleteException
-    {
-        writer.write("public %s read(JSONObject jsonObject) {", modelType.getParameterizedQualifiedSourceName());
-        writer.indent();
-        writer.write("%s model = null;", modelType.getParameterizedQualifiedSourceName());
-        writer.write("if (jsonObject != null) {");
-        writer.indent();
-        writer.write("model = internalRead(jsonObject);");
-        writer.outdent();
-        writer.write("}");
-        writer.write("return model;");
-        writer.outdent();
-        writer.write("}");
-    }
-
-
-    private void readListFromString(IndentedWriter writer) throws UnableToCompleteException
-    {
-        writer.write("public List<%s> readList(String jsonString) {", modelType.getParameterizedQualifiedSourceName());
-        writer.indent();
-        writer.write("List<%1$s> models = new ArrayList<%1$s>();", modelType.getParameterizedQualifiedSourceName());
-        writer.write("if (jsonString != null && jsonString.trim().length() != 0) {");
-        writer.indent();
-        writer.write("JSONValue jsonValue = JSONParser.parse(jsonString);");
-        writer.write("if (jsonValue != null) {");
-        writer.indent();
-        writer.write("JSONArray jsonArray = jsonValue.isArray();");
-        writer.write("if (jsonArray != null) {");
-        writer.indent();
-        writer.write("int size = jsonArray.size();");
-        writer.write("for (int i = 0; i < size; i++) {");
-        writer.indent();
-        writer.write("JSONValue currentJsonValue = jsonArray.get(i);");
-        writer.write("if (currentJsonValue != null) {");
-        writer.indent();
-        writer.write("JSONObject currentJsonObject = currentJsonValue.isObject();");
-        writer.write("if (currentJsonObject != null) {");
-        writer.indent();
-        writer.write("%s model = internalRead(currentJsonObject);", modelType.getParameterizedQualifiedSourceName());
-        writer.write("if (model != null) {");
-        writer.indent();
-        writer.write("models.add(model);");
-        writer.outdent();
-        writer.write("}");
-        writer.outdent();
-        writer.write("}");
-        writer.outdent();
-        writer.write("}");
-        writer.outdent();
-        writer.write("}");
-        writer.outdent();
-        writer.write("}");
-        writer.outdent();
-        writer.write("}");
-        writer.outdent();
-        writer.write("}");
-        writer.write("return models;");
-        writer.outdent();
-        writer.write("}");
-    }
-
-
-    private void readListFromJsonArray(IndentedWriter writer) throws UnableToCompleteException
-    {
-        writer
-                .write("public List<%s> readList(JSONArray jsonArray) {", modelType
-                        .getParameterizedQualifiedSourceName());
-        writer.indent();
-        writer.write("List<%1$s> models = new ArrayList<%1$s>();", modelType.getParameterizedQualifiedSourceName());
-        writer.write("if (jsonArray != null) {");
-        writer.indent();
-        writer.write("int size = jsonArray.size();");
-        writer.write("for (int i = 0; i < size; i++) {");
-        writer.indent();
-        writer.write("JSONValue currentJsonValue = jsonArray.get(i);");
-        writer.write("if (currentJsonValue != null) {");
-        writer.indent();
-        writer.write("JSONObject currentJsonObject = currentJsonValue.isObject();");
-        writer.write("if (currentJsonObject != null) {");
-        writer.indent();
-        writer.write("%s model = internalRead(currentJsonObject);", modelType.getParameterizedQualifiedSourceName());
-        writer.write("if (model != null) {");
-        writer.indent();
-        writer.write("models.add(model);");
-        writer.outdent();
-        writer.write("}");
-        writer.outdent();
-        writer.write("}");
-        writer.outdent();
-        writer.write("}");
-        writer.outdent();
-        writer.write("}");
-        writer.outdent();
-        writer.write("}");
-        writer.write("return models;");
-        writer.outdent();
-        writer.write("}");
-    }
-
-
-    private void internalRead(IndentedWriter writer) throws UnableToCompleteException
-    {
-        writer.write("private %s internalRead(JSONObject jsonObject) {", modelType
-                .getParameterizedQualifiedSourceName());
-        writer.indent();
-        writer.write("%1$s model = new %1$s();", modelType.getParameterizedQualifiedSourceName());
-        processFields(writer, modelType.getFields(), "jsonObject");
-        writer.write("return model;");
-        writer.outdent();
-        writer.write("}");
-    }
-
-
-    // ---------------------------------------------------------- field methods
-
-    private void processFields(IndentedWriter writer, JField[] fields, String jsonVariable)
-            throws UnableToCompleteException
-    {
-        if (fields != null && fields.length != 0)
+        JsonModel jsonModel = modelType.getAnnotation(JsonModel.class);
+        if (jsonModel != null)
         {
-            int counter = 0;
-            for (JField field : fields)
+            JsonField[] fields = jsonModel.value();
+            if (fields != null && fields.length != 0)
             {
-                JsonField jsonField = field.getAnnotation(JsonField.class);
-                if (jsonField != null)
+                int counter = 0;
+                for (JsonField jsonField : fields)
                 {
                     writer.newline();
-                    String jsonPath = calculateJsonPath(field, jsonField);
+                    JClassType fieldType = getFieldType(jsonField);
+                    String jsonPath = calculateJsonPath(jsonField);
                     FieldContext fieldContext = new FieldContext(context.getTypeOracle(), handlerRegistry, modelType,
-                            field.getType(), field.getName(), jsonPath, jsonField.format(), jsonVariable, "value"
+                            fieldType, jsonField.property(), jsonPath, jsonField.format(), jsonVariable, "value"
                                     + counter);
                     FieldHandler handler = handlerRegistry.findFieldHandler(fieldContext);
                     if (handler != null)
@@ -250,12 +80,23 @@ public class JsonModelReaderCreator extends AbstractReaderCreator
     }
 
 
-    private String calculateJsonPath(JField field, JsonField jsonField)
+    private JClassType getFieldType(JsonField jsonField) throws UnableToCompleteException
     {
-        String jsonPath = jsonField.value();
+        JClassType fieldType = context.getTypeOracle().findType(jsonField.type().getName());
+        if (fieldType == null)
+        {
+            die("Cannot find type {0}", jsonField.type().getName());
+        }
+        return fieldType;
+    }
+
+
+    private String calculateJsonPath(JsonField jsonField)
+    {
+        String jsonPath = jsonField.path();
         if (jsonPath == null || jsonPath.length() == 0)
         {
-            jsonPath = field.getName();
+            jsonPath = jsonField.property();
         }
         return jsonPath;
     }
--- gxt/src/main/java/name/pehl/piriti/gxt/rebind/xml/XmlModelReaderCreator.java
+++ gxt/src/main/java/name/pehl/piriti/gxt/rebind/xml/XmlModelReaderCreator.java
@@ -1,18 +1,18 @@
 package name.pehl.piriti.gxt.rebind.xml;
 
-import name.pehl.piriti.client.xml.XmlField;
-import name.pehl.piriti.rebind.AbstractReaderCreator;
+import name.pehl.piriti.gxt.client.xml.XmlField;
+import name.pehl.piriti.gxt.client.xml.XmlModel;
 import name.pehl.piriti.rebind.FieldContext;
+import name.pehl.piriti.rebind.FieldHandlerRegistry;
 import name.pehl.piriti.rebind.IndentedWriter;
 import name.pehl.piriti.rebind.TypeUtils;
 import name.pehl.piriti.rebind.fieldhandler.FieldHandler;
+import name.pehl.piriti.rebind.xml.XmlReaderCreator;
 
 import com.google.gwt.core.ext.GeneratorContext;
 import com.google.gwt.core.ext.TreeLogger;
 import com.google.gwt.core.ext.UnableToCompleteException;
 import com.google.gwt.core.ext.typeinfo.JClassType;
-import com.google.gwt.core.ext.typeinfo.JField;
-import com.google.gwt.core.ext.typeinfo.JType;
 
 /**
  * Class which generates the code necessary to map the annotated fields.
@@ -20,13 +20,19 @@ import com.google.gwt.core.ext.typeinfo.JType;
  * @author $LastChangedBy: harald.pehl $
  * @version $LastChangedRevision: 139 $
  */
-public class XmlModelReaderCreator extends AbstractReaderCreator
+public class XmlModelReaderCreator extends XmlReaderCreator
 {
     public XmlModelReaderCreator(GeneratorContext context, JClassType interfaceType, String implName,
             String readerClassname, TreeLogger logger) throws UnableToCompleteException
     {
         super(context, interfaceType, implName, readerClassname, logger);
-        this.handlerRegistry = new XmlModelFieldHandlerRegistry();
+    }
+
+
+    @Override
+    protected FieldHandlerRegistry setupFieldHandlerRegistry()
+    {
+        return new XmlModelFieldHandlerRegistry();
     }
 
 
@@ -34,136 +40,27 @@ public class XmlModelReaderCreator extends AbstractReaderCreator
     protected void createImports(IndentedWriter writer) throws UnableToCompleteException
     {
         super.createImports(writer);
-        writer.write("import com.google.gwt.xml.client.Document;");
-        writer.write("import com.google.gwt.xml.client.Element;");
-        writer.write("import name.pehl.piriti.client.xml.*;");
+        writer.write("com.extjs.gxt.ui.client.data.*;");
     }
 
 
     @Override
-    protected void createMemberVariables(IndentedWriter writer) throws UnableToCompleteException
+    protected void processMappings(IndentedWriter writer, String xmlVariable) throws UnableToCompleteException
     {
-        super.createMemberVariables(writer);
-        writer.write("private XmlRegistry xmlRegistry;");
-    }
-
-
-    @Override
-    protected void createConstructorBody(IndentedWriter writer)
-    {
-        super.createConstructorBody(writer);
-        writer.write("this.xmlRegistry = XmlGinjector.INJECTOR.getXmlRegistry();");
-        writer.write("this.xmlRegistry.register(%s.class, this);", modelType.getQualifiedSourceName());
-    }
-
-
-    @Override
-    protected void createMethods(IndentedWriter writer) throws UnableToCompleteException
-    {
-        readFromDocument(writer);
-        writer.newline();
-
-        readFromElement(writer);
-        writer.newline();
-
-        readListFromDocument(writer);
-        writer.newline();
-
-        readListFromElement(writer);
-        writer.newline();
-    }
-
-
-    private void readFromDocument(IndentedWriter writer) throws UnableToCompleteException
-    {
-        read(writer, "Document", "document");
-    }
-
-
-    private void readFromElement(IndentedWriter writer) throws UnableToCompleteException
-    {
-        read(writer, "Element", "element");
-    }
-
-
-    private void read(IndentedWriter writer, String xmlType, String xmlVariable) throws UnableToCompleteException
-    {
-        writer.write("public %s read(%s %s) {", modelType.getParameterizedQualifiedSourceName(), xmlType,
-                xmlVariable);
-        writer.indent();
-        writer.write("%s model = null;", modelType.getParameterizedQualifiedSourceName());
-        writer.write("if (%s != null) {", xmlVariable);
-        writer.indent();
-        writer.write("model = new %s();", modelType.getParameterizedQualifiedSourceName());
-        processFields(writer, modelType.getFields(), xmlVariable);
-        writer.outdent();
-        writer.write("}");
-        writer.write("return model;");
-        writer.outdent();
-        writer.write("}");
-    }
-
-
-    private void readListFromDocument(IndentedWriter writer) throws UnableToCompleteException
-    {
-        readList(writer, "Document", "document");
-    }
-
-
-    private void readListFromElement(IndentedWriter writer) throws UnableToCompleteException
-    {
-        readList(writer, "Element", "element");
-    }
-
-
-    private void readList(IndentedWriter writer, String xmlType, String xmlVariable) throws UnableToCompleteException
-    {
-        writer.write("public List<%s> readList(%s %s, String xpath) {",
-                modelType.getParameterizedQualifiedSourceName(), xmlType, xmlVariable);
-        writer.indent();
-        writer.write("List<%1$s> models = new ArrayList<%1$s>();", modelType.getParameterizedQualifiedSourceName());
-        writer.write("if (%s != null && xpath != null && xpath.length() != 0) {", xmlVariable);
-        writer.indent();
-        writer.write("List<Element> elements = XPathUtils.getElements(%s, xpath);", xmlVariable);
-        writer.write("if (elements != null && !elements.isEmpty()) {");
-        writer.indent();
-        writer.write("for (Element currentElement : elements) {");
-        writer.indent();
-        writer.write("%s model = read(currentElement);", modelType.getParameterizedQualifiedSourceName());
-        writer.write("if (model != null) {");
-        writer.indent();
-        writer.write("models.add(model);");
-        writer.outdent();
-        writer.write("}");
-        writer.outdent();
-        writer.write("}");
-        writer.outdent();
-        writer.write("}");
-        writer.outdent();
-        writer.write("}");
-        writer.write("return models;");
-        writer.outdent();
-        writer.write("}");
-    }
-
-
-    // ---------------------------------------------------------- field methods
-
-    private void processFields(IndentedWriter writer, JField[] fields, String xmlVariable)
-            throws UnableToCompleteException
-    {
-        if (fields != null && fields.length != 0)
+        XmlModel xmlModel = modelType.getAnnotation(XmlModel.class);
+        if (xmlModel != null)
         {
-            int counter = 0;
-            for (JField field : fields)
+            XmlField[] fields = xmlModel.value();
+            if (fields != null && fields.length != 0)
             {
-                XmlField xmlField = field.getAnnotation(XmlField.class);
-                if (xmlField != null)
+                int counter = 0;
+                for (XmlField xmlField : fields)
                 {
                     writer.newline();
-                    String xpath = calculateXpath(field, xmlField);
+                    JClassType fieldType = getFieldType(xmlField);
+                    String xpath = calculateXpath(fieldType, xmlField);
                     FieldContext fieldContext = new FieldContext(context.getTypeOracle(), handlerRegistry, modelType,
-                            field.getType(), field.getName(), xpath, xmlField.format(), xmlVariable, "value" + counter);
+                            fieldType, xmlField.property(), xpath, xmlField.format(), xmlVariable, "value" + counter);
                     FieldHandler handler = handlerRegistry.findFieldHandler(fieldContext);
                     if (handler != null)
                     {
@@ -183,13 +80,23 @@ public class XmlModelReaderCreator extends AbstractReaderCreator
     }
 
 
-    private String calculateXpath(JField field, XmlField xmlField)
+    private JClassType getFieldType(XmlField xmlField) throws UnableToCompleteException
     {
-        String xpath = xmlField.value();
+        JClassType fieldType = context.getTypeOracle().findType(xmlField.type().getName());
+        if (fieldType == null)
+        {
+            die("Cannot find type {0}", xmlField.type().getName());
+        }
+        return fieldType;
+    }
+
+
+    private String calculateXpath(JClassType fieldType, XmlField xmlField)
+    {
+        String xpath = xmlField.path();
         if (xpath == null || xpath.length() == 0)
         {
-            xpath = field.getName();
-            JType fieldType = field.getType();
+            xpath = xmlField.property();
             if (fieldType.isPrimitive() != null || TypeUtils.isBasicType(fieldType) || fieldType.isEnum() != null)
             {
                 xpath += "/text()";
