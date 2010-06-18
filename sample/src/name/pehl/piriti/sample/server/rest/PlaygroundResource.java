06abfc96e7914e3c29af8804ca8f7f71c6e370c1
Harald Pehl <harald.pehl@gmail.com>
2010-06-18 14:57:55 +0000

    Moved common code to BaseResource.java

--- sample/src/name/pehl/piriti/sample/server/rest/PlaygroundResource.java
+++ sample/src/name/pehl/piriti/sample/server/rest/PlaygroundResource.java
@@ -1,33 +1,24 @@
 package name.pehl.piriti.sample.server.rest;
 
 import java.io.IOException;
-import java.io.InputStream;
 
 import org.restlet.data.MediaType;
-import org.restlet.representation.InputRepresentation;
 import org.restlet.representation.Representation;
 import org.restlet.resource.Get;
-import org.restlet.resource.ServerResource;
 
 /**
+ * Resource to test arbitrary XML which is loaded from
+ * <code>name/pehl/piriti/sample/server/rest/playground.xml</code>.
+ * 
  * @author $Author: harald.pehl $
  * @version $Date: 2010-03-25 15:16:49 +0100 (Do, 25 Mrz 2010) $ $Revision: 272
  *          $
  */
-public class PlaygroundResource extends ServerResource
+public class PlaygroundResource extends BaseResource
 {
     @Get("xml")
     public Representation represent() throws IOException
     {
         return loadResource("name/pehl/piriti/sample/server/rest/playground.xml", MediaType.TEXT_XML);
-    }
-
-
-    private Representation loadResource(String resource, MediaType mediaType)
-    {
-        ClassLoader contextClassLoader = Thread.currentThread().getContextClassLoader();
-        InputStream stream = contextClassLoader.getResourceAsStream(resource);
-        InputRepresentation representation = new InputRepresentation(stream, mediaType);
-        return representation;
     }
 }
