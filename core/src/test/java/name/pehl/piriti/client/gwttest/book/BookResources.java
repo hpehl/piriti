abbd9e609533fd239fafbc407b7695ce9e507fd6
Harald Pehl <harald.pehl@gmail.com>
2010-06-21 22:03:51 +0000

    Added resources for gwt unit tests

---
+++ core/src/test/java/name/pehl/piriti/client/gwttest/book/BookResources.java
@@ -0,0 +1,47 @@
+package name.pehl.piriti.client.gwttest.book;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.resources.client.ClientBundle;
+import com.google.gwt.resources.client.TextResource;
+
+/**
+ * @author $LastChangedBy:$
+ * @version $LastChangedRevision:$
+ */
+
+public interface BookResources extends ClientBundle
+{
+    // -------------------------------------------------------------- constants
+
+    String BOOKS = "books";
+    int BOOKS_COUNT = 3;
+
+    String ISBN = "978-0345417954";
+    int PAGES = 432;
+    String TITLE = "The Hotel New Hampshire";
+    String AUTHOR_SURNAME = "Irving";
+    String AUTHOR_FIRSTNAME = "John";
+    String[] REVIEWS = new String[] {"A hectic gaudy saga with the verve of a Marx Brothers movie.",
+            "Rejoice! John Irving has written another book according to your world. You must read this book.",
+            "Spellbinding, intensely human, a high-wire act of dazzling virtuosity.",};
+
+    // ------------------------------------------------------- deferred binding
+
+    BookResources INSTANCE = GWT.create(BookResources.class);
+
+
+    @Source("book.json")
+    public TextResource bookJson();
+
+
+    @Source("books.json")
+    public TextResource booksJson();
+
+
+    @Source("book.xml")
+    public TextResource bookXml();
+
+
+    @Source("books.xml")
+    public TextResource booksXml();
+}
