e36524be85c8c2130567fe6413e84f152847645b
Harald Pehl <harald.pehl@gmail.com>
2010-06-24 12:27:25 +0000

    Prepared Sarissa XPath implementation

---
+++ core/src/main/java/name/pehl/piriti/client/xml/internal/SarissaXPathImpl.java
@@ -0,0 +1,83 @@
+package name.pehl.piriti.client.xml.internal;
+
+import java.util.List;
+
+import name.pehl.piriti.client.xml.AbstractXPathImpl;
+
+import com.google.gwt.xml.client.Document;
+import com.google.gwt.xml.client.Element;
+
+/**
+ * {@link name.pehl.piriti.client.xml.XPath} implementation using <a
+ * href="http://dev.abiss.gr/sarissa/">Sarissa</a>.
+ * <p>
+ * Supports namespaces!
+ * 
+ * @author $Author:$
+ * @version $Date:$ $Revision:$
+ */
+public class SarissaXPathImpl extends AbstractXPathImpl
+{
+    @Override
+    public Element getElement(Document document, String xpath)
+    {
+        // TODO Implement me!
+        throw new UnsupportedOperationException("Not implemented");
+    }
+
+
+    @Override
+    public Element getElement(Element element, String xpath)
+    {
+        // TODO Implement me!
+        throw new UnsupportedOperationException("Not implemented");
+    }
+
+
+    @Override
+    public List<Element> getElements(Document document, String xpath)
+    {
+        // TODO Implement me!
+        throw new UnsupportedOperationException("Not implemented");
+    }
+
+
+    @Override
+    public List<Element> getElements(Element element, String xpath)
+    {
+        // TODO Implement me!
+        throw new UnsupportedOperationException("Not implemented");
+    }
+
+
+    @Override
+    public String getValue(Document document, String xpath)
+    {
+        // TODO Implement me!
+        throw new UnsupportedOperationException("Not implemented");
+    }
+
+
+    @Override
+    public String getValue(Element element, String xpath)
+    {
+        // TODO Implement me!
+        throw new UnsupportedOperationException("Not implemented");
+    }
+
+
+    @Override
+    public String[] getValues(Document document, String xpath)
+    {
+        // TODO Implement me!
+        throw new UnsupportedOperationException("Not implemented");
+    }
+
+
+    @Override
+    public String[] getValues(Element element, String xpath)
+    {
+        // TODO Implement me!
+        throw new UnsupportedOperationException("Not implemented");
+    }
+}
