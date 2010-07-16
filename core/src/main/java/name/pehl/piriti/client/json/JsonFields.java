9b988a92430e03d1813ec7755043be46ef795cc8
Harald Pehl <harald.pehl@gmail.com>
2010-07-16 22:06:17 +0000

    Javadoc

--- core/src/main/java/name/pehl/piriti/client/json/JsonFields.java
+++ core/src/main/java/name/pehl/piriti/client/json/JsonFields.java
@@ -7,13 +7,38 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * @author $LastChangedBy:$
- * @version $LastChangedRevision:$
+ * Annotation which can be used in case the POJOs fields cannot be annotated
+ * direclty. The annotation has to be placed at the interface which extends
+ * {@link JsonReader}:
+ * 
+ * <pre>
+ * public class Readers
+ * {
+ *     &#64;JsonFields({&#64;JsonField(name = "id"), 
+ *         &#64;JsonField(name = "firstname"), &#64;JsonField(name = "surname")})
+ *     public interface CustomerReader extends JsonReader&lt;Customer&gt; { }
+ *     public static final CustomerReader CUSTOMER = GWT.create(CustomerReader.class);
+ *     
+ *     // other readers
+ *     ...
+ * }
+ * 
+ * String json = ...;
+ * Customer c = Readers.CUSTOMER.read(json);
+ * </pre>
+ * 
+ * @author $LastChangedBy$
+ * @version $LastChangedRevision$
  */
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
 public @interface JsonFields
 {
+    /**
+     * The mapping definitions for the POJOs fields.
+     * 
+     * @return
+     */
     JsonField[] value();
 }
--- core/src/main/java/name/pehl/piriti/client/xml/XmlFields.java
+++ core/src/main/java/name/pehl/piriti/client/xml/XmlFields.java
@@ -7,6 +7,26 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
+ * Annotation which can be used in case the POJOs fields cannot be annotated
+ * direclty. The annotation has to be placed at the interface which extends
+ * {@link XmlReader}:
+ * 
+ * <pre>
+ * public class Readers
+ * {
+ *     &#64;XmlFields(id = &#64;XmlId(name = "id"), 
+ *         value = {&#64;XmlField(name = "firstname"), &#64;XmlField(name = "surname")})
+ *     public interface CustomerReader extends XmlReader&lt;Customer&gt; { }
+ *     public static final CustomerReader CUSTOMER = GWT.create(CustomerReader.class);
+ *     
+ *     // other readers
+ *     ...
+ * }
+ * 
+ * Document document = ...;
+ * Customer c = Readers.CUSTOMER.read(document);
+ * </pre>
+ * 
  * @author $LastChangedBy$
  * @version $LastChangedRevision$
  */
