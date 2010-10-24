43ed915f0d045de02cbc602fb317f078b72f1f5b
Harald Pehl <harald.pehl@gmail.com>
2010-10-24 21:35:33 +0000

    Added hashCode() and equals()

--- core/src/main/java/name/pehl/piriti/rebind/propertyhandler/PropertyAnnotation.java
+++ core/src/main/java/name/pehl/piriti/rebind/propertyhandler/PropertyAnnotation.java
@@ -29,6 +29,48 @@ public class PropertyAnnotation<T extends Annotation> implements Comparable<Prop
 
 
     @Override
+    public int hashCode()
+    {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + (property == null ? 0 : property.hashCode());
+        return result;
+    }
+
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public boolean equals(Object obj)
+    {
+        if (this == obj)
+        {
+            return true;
+        }
+        if (obj == null)
+        {
+            return false;
+        }
+        if (getClass() != obj.getClass())
+        {
+            return false;
+        }
+        PropertyAnnotation<T> other = (PropertyAnnotation<T>) obj;
+        if (property == null)
+        {
+            if (other.property != null)
+            {
+                return false;
+            }
+        }
+        else if (!property.equals(other.property))
+        {
+            return false;
+        }
+        return true;
+    }
+
+
+    @Override
     public int compareTo(PropertyAnnotation<T> o)
     {
         return order - o.order;
