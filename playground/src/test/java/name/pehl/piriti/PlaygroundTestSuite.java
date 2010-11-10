ae6df682f7a716697a9b818359d3ddea98c119f5
Harald Pehl <harald.pehl@gmail.com>
2010-11-10 22:11:11 +0000

    Added simple test for JSONPath

--- playground/src/test/java/name/pehl/piriti/PlaygroundTestSuite.java
+++ playground/src/test/java/name/pehl/piriti/PlaygroundTestSuite.java
@@ -2,7 +2,7 @@ package name.pehl.piriti;
 
 import junit.framework.Test;
 import junit.framework.TestSuite;
-import name.pehl.piriti.client.gwttest.specialjsonkey.RequestTest;
+import name.pehl.piriti.client.gwttest.arcbees.BlogItemTest;
 import name.pehl.piriti.client.gwttest.ticketgoose.TicketgooseTest;
 
 import com.google.gwt.junit.tools.GWTTestSuite;
@@ -18,7 +18,7 @@ public class PlaygroundTestSuite extends GWTTestSuite
     {
         TestSuite suite = new TestSuite("GWT tests for piriti-playground");
 
-        suite.addTestSuite(RequestTest.class);
+        suite.addTestSuite(BlogItemTest.class);
         suite.addTestSuite(TicketgooseTest.class);
 
         return suite;
