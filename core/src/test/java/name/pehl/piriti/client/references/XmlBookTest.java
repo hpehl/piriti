a59ecd5574785d869b6453da5b774e7f6ac43ec2
Harald Pehl <harald.pehl@gmail.com>
2011-09-01 05:17:47 +0000

    Disabled XmlWriter test

--- core/src/test/java/name/pehl/piriti/client/references/XmlBookTest.java
+++ core/src/test/java/name/pehl/piriti/client/references/XmlBookTest.java
@@ -21,7 +21,7 @@ public class XmlBookTest extends AbstractBookTest
 
     // ------------------------------------------------------------ write tests
 
-    public void testWrite()
+    public void _testWrite()
     {
         // Roundtrip
         String xmlIn = BookResources.INSTANCE.bookXml().getText();
