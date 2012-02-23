2afb1cc8e7376340c2850aa056d64387bf2bb57a
Harald Pehl <harald.pehl@gmail.com>
2012-02-23 15:52:16 +0000

    Preparing Guice DI.

--- dev/src/main/java/name/pehl/piriti/rebind/RebindModule.java
+++ dev/src/main/java/name/pehl/piriti/rebind/RebindModule.java
@@ -1,8 +1,20 @@
 package name.pehl.piriti.rebind;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+import name.pehl.piriti.rebind.type.PojoTypeProcessor;
+import name.pehl.piriti.rebind.type.RwTypeProcessor;
+import name.pehl.piriti.rebind.type.TypeProcessor;
+
+import org.apache.velocity.app.VelocityEngine;
+
 import com.google.gwt.core.ext.GeneratorContext;
 import com.google.gwt.core.ext.TreeLogger;
+import com.google.gwt.core.ext.UnableToCompleteException;
 import com.google.inject.AbstractModule;
+import com.google.inject.Provides;
 
 public class RebindModule extends AbstractModule
 {
@@ -21,5 +33,35 @@ public class RebindModule extends AbstractModule
     @Override
     protected void configure()
     {
+        bindConstant().annotatedWith(VelocityProperties.class).to("name/pehl/piriti/rebind/velocity.properties");
+    }
+
+
+    @Provides
+    TypeProcessor provideTypeProcessor()
+    {
+        TypeProcessor typeProcessor = new PojoTypeProcessor();
+        typeProcessor.setNext(new RwTypeProcessor());
+        return typeProcessor;
+    }
+
+
+    @Provides
+    VelocityEngine provideVelocityEngine(@VelocityProperties String velocityProperties)
+            throws UnableToCompleteException
+    {
+        VelocityEngine engine = null;
+        InputStream inputStream = this.getClass().getClassLoader().getResourceAsStream(velocityProperties);
+        Properties properties = new Properties();
+        try
+        {
+            properties.load(inputStream);
+            engine = new VelocityEngine(properties);
+        }
+        catch (IOException e)
+        {
+            Logger.get().die("Cannot load velocity properties from " + velocityProperties);
+        }
+        return engine;
     }
 }
---
+++ dev/src/main/java/name/pehl/piriti/rebind/VelocityProperties.java
@@ -0,0 +1,16 @@
+package name.pehl.piriti.rebind;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import com.google.inject.BindingAnnotation;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.FIELD, ElementType.PARAMETER})
+@BindingAnnotation
+public @interface VelocityProperties
+{
+
+}
